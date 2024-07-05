78593a980bc1dc4c159839c3a4ebf3999166c6f9
Francisco <javiermartincuenca@gmail.com>
2024-07-05 18:15:33 +0200

    Add prettier-plugin-tailwindcss

--- src/components/wrapper/app-container.vue
+++ src/components/wrapper/app-container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center items-center size-full">
+  <div class="flex size-full items-center justify-center">
     <div class="screen">
       <slot />
     </div>
