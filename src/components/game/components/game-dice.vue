cd5e5b6859f87b791f29fed881b4bbfb29039478
Francisco <javiermartincuenca@gmail.com>
2024-07-06 12:13:21 +0200

    style: add 1 point dice

--- src/components/game/components/game-dice.vue
+++ src/components/game/components/game-dice.vue
@@ -15,11 +15,37 @@ interface DiceProps {
   animate?: boolean;
 }
 
-withDefaults(defineProps<DiceProps>(), { size: 30, animate: false });
+withDefaults(defineProps<DiceProps>(), { size: 130, animate: false });
 </script>
 
 <style scoped>
-.game-dice{
+.game-dice {
+  --point: radial-gradient(circle, black 35%, rgba(255, 255, 255, 0) 50%);
 
+  background-color: white;
+  border-radius: 2px;
+  position: relative;
+  box-shadow:
+    rgba(0, 0, 0, 0.4) 0px 2px 4px,
+    rgba(0, 0, 0, 0.3) 0px 7px 13px -3px,
+    rgba(0, 0, 0, 0.2) 0px -1px 0px inset;
+
+  &::after {
+    background-repeat: no-repeat;
+    background-size: 30% 30%;
+    content: '';
+    filter: drop-shadow(0 0 1px black);
+    height: 100%;
+    left: 0;
+    overflow: hidden;
+    position: absolute;
+    top: 0;
+    width: 100%;
+  }
+
+  &.dice-1::after {
+    background-image: var(--point);
+    background-position: 50% 50%;
+  }
 }
 </style>
