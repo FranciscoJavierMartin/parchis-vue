7998beb62abb072394ada68a3c4f07626153ae3f
Francisco <javiermartincuenca@gmail.com>
2024-07-06 12:03:14 +0200

    feat: create dice component

---
+++ src/components/game/components/game-dice.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="game-dice"
+    :class="[`dice-${value}`, { animate }]"
+    :style="{ width: `${size}px`, height: `${size}px` }"
+  />
+</template>
+
+<script setup lang="ts">
+import type { TDiceValues } from '@/interfaces';
+
+interface DiceProps {
+  value: TDiceValues;
+  size?: number;
+  animate?: boolean;
+}
+
+withDefaults(defineProps<DiceProps>(), { size: 30, animate: false });
+</script>
+
+<style scoped>
+.game-dice{
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <template>
   <AppWrapper>
-    <GameBoard />
+    <!-- <GameBoard /> -->
+    <GameDice :value="1" />
   </AppWrapper>
 </template>
 
 <script setup lang="ts">
 import GameBoard from '@/components/game/components/game-board.vue';
+import GameDice from '@/components/game/components/game-dice.vue';
 import AppWrapper from '@/components/wrapper/app/app-wrapper.vue';
 </script>
