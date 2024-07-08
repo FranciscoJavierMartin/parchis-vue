7ce57f552e2e7a04a7c556e3dbe8400166e2332e
Francisco <javiermartincuenca@gmail.com>
2024-07-08 21:30:58 +0200

    feat: add game avatar component

---
+++ src/components/avatar/game-avatar.vue
@@ -0,0 +1,28 @@
+<template>
+  <img
+    :src="photo || defaultImage"
+    :alt="name"
+    :class="[className]"
+    @error="
+      (event: Event) => {
+        (event.currentTarget as any).onerror = null;
+        (event.currentTarget as any).src = '@/assets/default.png';
+      }
+    "
+  />
+</template>
+
+<script lang="ts" setup>
+import defaultImage from '@/assets/default.png';
+interface GameAvatarProps {
+  photo?: string;
+  name?: string;
+  className?: string;
+}
+
+withDefaults(defineProps<GameAvatarProps>(), {
+  photo: '',
+  name: '',
+  className: '',
+});
+</script>
