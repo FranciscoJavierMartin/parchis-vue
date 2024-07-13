d867b6964e6f0d6f155997deb4cb9f378ca7c0bb
Francisco <javiermartincuenca@gmail.com>
2024-07-13 12:54:36 +0200

    chore: add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
+import OfflineView from '@/views/OfflineView.vue';
+import OnlineView from '@/views/OnlineView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,21 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/offline',
+      name: 'offline',
+      component: OfflineView,
+    },
+    {
+      path: '/online',
+      name: 'online',
+      component: OnlineView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/views/OnlineView.vue'),
     },
   ],
 });
