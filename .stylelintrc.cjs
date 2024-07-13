2eddbd6170ebccf5083b1ba86ab198d1176b422e
Francisco <javiermartincuenca@gmail.com>
2024-07-13 09:14:39 +0200

    chore: setup stylelint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  // Register the prettier plugin for stylelint.
+  plugins: ['stylelint-prettier'],
+  // Inherit a set of rule collections.
+  extends: [
+    // standard rule set
+    'stylelint-config-standard',
+    // Style property order rules
+    'stylelint-config-recess-order',
+    // Incorporate Prettier rules
+    'stylelint-config-prettier',
+    'stylelint-prettier/recommended',
+  ],
+  // Configure rules
+  rules: {
+    // Enable Prettier auto-formatting
+    'prettier/prettier': true,
+  },
+};
