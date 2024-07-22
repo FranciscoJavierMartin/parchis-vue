0fb2e81a9a98846d1700bec89e4f34c231a0705f
Francisco <javiermartincuenca@gmail.com>
2024-07-22 22:39:46 +0200

    refactor: move to layout

--- src/views/OfflineView.vue
+++ src/views/OfflineView.vue
@@ -1,20 +1,20 @@
 <template>
-  <AppWrapper>
+  <PageWrapper>
     <Game
       :debug="envVars.debug"
       :users="TEMP_USERS"
       :initial-turn="0"
       :total-players="totalPlayers"
     />
-  </AppWrapper>
+  </PageWrapper>
 </template>
 
 <script setup lang="ts">
 import Game from '@/components/game/game.vue';
-import AppWrapper from '@/components/wrapper/app-wrapper.vue';
 import type { TTotalPlayers } from '@/interfaces/game';
 import { TEMP_USERS } from '@/helpers/player';
 import { envVars } from '@/config/envVar';
+import PageWrapper from '@/layout/page-wrapper.vue';
 
 // TODO: Use users.length instead of pass totalPlayers
 const totalPlayers = TEMP_USERS.length as TTotalPlayers;
