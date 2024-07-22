a4c56ceb6765a91eb65150b9725dfdd931700d82
Francisco <javiermartincuenca@gmail.com>
2024-07-22 19:49:05 +0200

    feat: add indicator

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <RouterView />
+  <Suspense>
+    <RouterView />
+    <template #fallback>
+      <LoadingIndicator />
+    </template>
+  </Suspense>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import LoadingIndicator from '@/components/common/loading-indicator.vue';
 </script>
