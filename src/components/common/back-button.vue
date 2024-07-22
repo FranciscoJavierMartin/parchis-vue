951b0aa7ec8f1ab270a08568e5cf008e5de27c01
Francisco <javiermartincuenca@gmail.com>
2024-07-22 23:30:44 +0200

    chore: wip

--- src/components/common/back-button.vue
+++ src/components/common/back-button.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>Back button</div>
+  <button v-if="withConfirmation" class="button blue game-back-button">
+    <ArrowLeft />
+  </button>
+  <RouterLink v-else :to="{ name: to }" class="button blue game-back-button">
+    <ArrowLeft />
+  </RouterLink>
 </template>
 
 <script setup lang="ts">
 import type { TRoutesName } from '@/interfaces/routes';
+import { useRouter } from 'vue-router';
+import ArrowLeft from '@/components/icons/arrow-left.vue';
+const router = useRouter();
 
 interface BackButtonProps {
   to?: TRoutesName;
+  withConfirmation?: boolean;
 }
 
-defineProps<BackButtonProps>();
+withDefaults(defineProps<BackButtonProps>(), { to: 'home', withConfirmation: false });
 </script>
+
+<style scoped>
+.game-back-button {
+}
+</style>
