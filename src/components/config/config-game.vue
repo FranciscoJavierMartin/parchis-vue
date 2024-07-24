86b9890ff01b793019737f4a99ffc5d8380041ab
Francisco <javiermartincuenca@gmail.com>
2024-07-24 22:28:09 +0200

    chore: setup player input

--- src/components/config/config-game.vue
+++ src/components/config/config-game.vue
@@ -1,7 +1,9 @@
 <template>
   <form @submit="handleSubmit" class="game-offline glass-effect">
     <SelectNumberPlayers v-model="totalPlayers" />
-    <div class="game-offline-players"></div>
+    <div class="game-offline-players">
+      <PlayerInput v-for="player in players" :key="player.id" v-bind="player" />
+    </div>
     <button type="submit" class="button yellow game-offline-play">
       <PlayIcon fill="#8b5f00" />
       Play
@@ -13,6 +15,7 @@
 import { ref } from 'vue';
 import PlayIcon from '@/components/icons/play-icon.vue';
 import SelectNumberPlayers from '@/components/base/select-number-players.vue';
+import PlayerInput from '@/components/config/player-input.vue';
 import type { DataOfflineGame, TTotalPlayers } from '@/interfaces/game';
 import { getInitialDataOfflinePlayers, getInitialTotalPlayers } from '@/helpers/player';
 import type { IPlayerOffline } from '@/interfaces/player';
@@ -39,6 +42,14 @@ function handleSubmit(event: Event): void {
   margin-top: 40px;
   border-radius: 16px;
   animation: fadeInLeft 0.5s ease both;
+
+  .game-offline-players {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
 
   .game-offline-play {
     display: flex;
---
+++ src/components/config/player-input.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="game-offline-player">
+    {{ color }}
+    {{ name }}
+    {{ isBot ? 'Yes' : 'No' }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { TColors } from '@/interfaces/board';
+import type { IPlayerOffline } from '@/interfaces/player';
+
+interface PlayerInputProps extends IPlayerOffline {}
+
+withDefaults(defineProps<PlayerInputProps>(), { name: '', disabled: false });
+
+defineEmits<{
+  isBot: [nextChecked: boolean];
+  updateName: [name: string];
+  changeColor: [color: TColors];
+}>();
+</script>
+
+<style scoped>
+.game-offline-player {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  margin-bottom: 15px;
+}
+</style>
