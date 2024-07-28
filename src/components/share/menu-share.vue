ba90e524095ba1cf25dcd2e99f826cb16e73cb09
Francisco <javiermartincuenca@gmail.com>
2024-07-28 13:11:30 +0200

    chore: menu share added

---
+++ src/components/share/menu-share.vue
@@ -0,0 +1,16 @@
+<template>
+  <slot :onClick="onClick" />
+</template>
+
+<script lang="ts" setup>
+interface ShareProps {
+  data: ShareData;
+  useNativeOption?: boolean;
+}
+
+withDefaults(defineProps<ShareProps>(), { useNativeOption: true });
+
+function onClick(): void {
+  console.log('Hello');
+}
+</script>
