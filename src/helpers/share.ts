38eed968c9cdfe77e7b703a3fe0b1342f02391d3
Francisco <javiermartincuenca@gmail.com>
2024-07-28 13:36:50 +0200

    chore: add todos

--- src/helpers/share.ts
+++ src/helpers/share.ts
@@ -1,14 +1,14 @@
 export const SHARE_AVAILABLE: boolean = 'share' in navigator;
 
-export function successMessage() {}
-
 export function shareLink(data: ShareData): void {
   navigator
     .share(data)
     .then(() => {
+      // TODO: Display toast
       console.log('Suceess');
     })
     .catch(() => {
+      // TODO: Display toast
       console.log('Error');
     });
 }
