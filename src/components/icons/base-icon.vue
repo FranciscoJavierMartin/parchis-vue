1b3a22b86cb95730d10383b4d2e0ee85f5b663bc
Francisco <javiermartincuenca@gmail.com>
2024-07-29 19:36:01 +0200

    feat: add base icon component

---
+++ src/components/icons/base-icon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="icon-wrapper" :class="`icon-${type}`">
+    <svg :viewBox="showIcon.viewBox">
+      <path v-for="(d, key) of showIcon.path" :key="key" :d="d" :fill="fill" />
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { ICONS_DATA } from '@/constants/icons';
+import type { TypeIcon } from '@/interfaces/icons';
+
+interface GameIconProps {
+  type: TypeIcon;
+  fill?: string;
+}
+
+const props = withDefaults(defineProps<GameIconProps>(), { fill: 'white' });
+
+const showIcon = computed(() => ICONS_DATA[props.type]);
+</script>
+
+<style scoped>
+.icon-wrapper {
+  pointer-events: none;
+}
+</style>
--- src/interfaces/icons.ts
+++ src/interfaces/icons.ts
@@ -1,4 +1,9 @@
+import type { ICONS_DATA } from '@/constants/icons';
+
+// TODO: Remove
 export interface IconProps {
   fill?: string;
   strokeWidth?: number;
 }
+
+export type TypeIcon = keyof typeof ICONS_DATA;
