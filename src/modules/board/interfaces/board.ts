289f7adc8508c45c582a7d9a36b6def22778bbfa
Francisco <javiermartincuenca@gmail.com>
2024-07-31 21:02:18 +0200

    refactor: board types

--- src/modules/board/interfaces/board.ts
+++ src/modules/board/interfaces/board.ts
@@ -1,11 +1,3 @@
-import type {
-  EBoardColors,
-  EColors,
-  EPositionGame,
-  ESufixColors,
-  EtypeTile,
-} from '@board/constants/board';
-
 export type TBoardColors = keyof typeof EBoardColors;
 export type TColors = keyof typeof EColors;
 export type TSufixColors = keyof typeof ESufixColors;
@@ -43,3 +35,67 @@ export interface IPositionGame {
 }
 
 export type TLocationBoardElements = Record<TPositionGame, IPositionGame>;
+
+// Board orientations.
+/**
+ * RGYB -> Red, green, yellow and blue.
+ * BRGY -> Blue, red, green and yellow.
+ * YBRG -> Yellow, blue, red and green.
+ * GYBR -> Green, yellow, blue and red.
+ */
+export enum EBoardColors {
+  RGYB = 'RGYB',
+  BRGY = 'BRGY',
+  YBRG = 'YBRG',
+  GYBR = 'GYBR',
+}
+
+/**
+ * Colors used on board
+ */
+export enum EColors {
+  RED = 'RED',
+  BLUE = 'BLUE',
+  YELLOW = 'YELLOW',
+  GREEN = 'GREEN',
+}
+
+/**
+ * Store the color prefixes
+ */
+export enum ESufixColors {
+  R = 'RED',
+  B = 'BLUE',
+  Y = 'YELLOW',
+  G = 'GREEN',
+}
+
+/**
+ * Players location in board
+ */
+export enum EPositionGame {
+  BOTTOM_LEFT = 'BOTTOM_LEFT',
+  TOP_LEFT = 'TOP_LEFT',
+  TOP_RIGHT = 'TOP_RIGHT',
+  BOTTOM_RIGHT = 'BOTTOM_RIGHT',
+}
+
+export enum EPositionProfiles {
+  TOP = 'TOP',
+  BOTTOM = 'BOTTOM',
+}
+
+export enum EPositionProfile {
+  LEFT = 'LEFT',
+  RIGHT = 'RIGHT',
+}
+
+/**
+ * Tile types in board
+ */
+export enum EtypeTile {
+  JAIL = 'JAIL',
+  NORMAL = 'NORMAL',
+  EXIT = 'EXIT',
+  END = 'END',
+}
