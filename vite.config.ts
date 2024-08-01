4c3d9d08d598267f8ed0215b2d3bf169d1a51f55
Francisco <javiermartincuenca@gmail.com>
2024-08-01 09:59:44 +0200

    feat: setup path alias

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,64 @@ export default defineConfig({
   base: '/parchis-vue/',
   plugins: [vue(), vueDevTools()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      {
+        find: '@about',
+        replacement: fileURLToPath(new URL('./src/modules/about', import.meta.url)),
+      },
+      {
+        find: '@board',
+        replacement: fileURLToPath(new URL('./src/modules/board', import.meta.url)),
+      },
+      { find: '@chat', replacement: fileURLToPath(new URL('./src/modules/chat', import.meta.url)) },
+      {
+        find: '@common',
+        replacement: fileURLToPath(new URL('./src/modules/common', import.meta.url)),
+      },
+      {
+        find: '@debug',
+        replacement: fileURLToPath(new URL('./src/modules/debug', import.meta.url)),
+      },
+      { find: '@dice', replacement: fileURLToPath(new URL('./src/modules/dice', import.meta.url)) },
+      { find: '@game', replacement: fileURLToPath(new URL('./src/modules/game', import.meta.url)) },
+      { find: '@home', replacement: fileURLToPath(new URL('./src/modules/home', import.meta.url)) },
+      {
+        find: '@layouts',
+        replacement: fileURLToPath(new URL('./src/modules/layouts', import.meta.url)),
+      },
+      {
+        find: '@offline',
+        replacement: fileURLToPath(new URL('./src/modules/offline', import.meta.url)),
+      },
+      {
+        find: '@online',
+        replacement: fileURLToPath(new URL('./src/modules/online', import.meta.url)),
+      },
+      {
+        find: '@options',
+        replacement: fileURLToPath(new URL('./src/modules/options', import.meta.url)),
+      },
+      {
+        find: '@players',
+        replacement: fileURLToPath(new URL('./src/modules/players', import.meta.url)),
+      },
+      {
+        find: '@profiles',
+        replacement: fileURLToPath(new URL('./src/modules/profiles', import.meta.url)),
+      },
+      {
+        find: '@share',
+        replacement: fileURLToPath(new URL('./src/modules/share', import.meta.url)),
+      },
+      {
+        find: '@sounds',
+        replacement: fileURLToPath(new URL('./src/modules/sounds', import.meta.url)),
+      },
+      {
+        find: '@tokens',
+        replacement: fileURLToPath(new URL('./src/modules/tokens', import.meta.url)),
+      },
+    ],
   },
 });
