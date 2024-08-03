03d5c589fcee9d3f1a804c073481f0b0ef4fa1e4
Francisco <javiermartincuenca@gmail.com>
2024-08-03 19:01:55 +0200

    docs: add story for app-container

--- src/modules/layouts/components/wrapper/app-wrapper.vue
+++ src/modules/layouts/components/wrapper/app-wrapper.vue
@@ -7,6 +7,6 @@
 </template>
 
 <script setup lang="ts">
-import AppContainer from '@layouts/components/wrapper/app-container.vue';
+import AppContainer from '@layouts/components/wrapper/app-container/app-container.vue';
 import OptionsGameProvider from '@sounds/providers/options-game.provider';
 </script>
