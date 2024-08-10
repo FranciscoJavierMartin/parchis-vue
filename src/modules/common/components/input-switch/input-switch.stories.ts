91e0925a55e0e806ce905378915fa2a40d441e80
Francisco <javiermartincuenca@gmail.com>
2024-08-10 13:56:59 +0200

    feat: add component

---
+++ src/modules/common/components/input-switch/input-switch.stories.ts
@@ -0,0 +1,35 @@
+import { ref } from 'vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+import InputSwitch from '@common/components/input-switch/input-switch.vue';
+
+const meta: Meta<typeof InputSwitch> = {
+  title: 'Common/InputSwitch',
+  component: InputSwitch,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  args: {
+    id: '1234abcd',
+  },
+  decorators: [
+    (args: object): object => ({
+      setup(): object {
+        const value = ref<boolean>(false);
+
+        return { args, value };
+      },
+      template: `
+        <div style="margin: 1rem; display: flex; flex-direction: column; gap: 10px;">
+          <story v-model="value" />
+          <span style="color: blue; font-size: 20px;">{{ value.valueOf() }}</span>
+        </div>
+    `,
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
