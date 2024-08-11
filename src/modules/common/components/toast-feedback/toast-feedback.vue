b0c1fb404c069048f98f3fb6b1a98ea6e69ea990
Francisco <javiermartincuenca@gmail.com>
2024-08-11 23:33:19 +0200

    style: adjust width

--- src/modules/common/components/toast-feedback/toast-feedback.vue
+++ src/modules/common/components/toast-feedback/toast-feedback.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="toast-container">
     <ul class="toast-list">
+      <!-- <li class="toast-message">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed asperiores culpa eos autem
+        delectus vitae ratione odio maiores esse eum, incidunt praesentium quo eius architecto
+        deserunt. Sit velit rerum officiis!
+      </li> -->
       <li class="toast-message">Hello world</li>
     </ul>
   </div>
@@ -18,11 +23,17 @@ interface ToastFeedbackProps {
 .toast-container {
   position: absolute;
   bottom: 50px;
+  z-index: 20;
 
   .toast-list {
+    max-width: 80%;
+    margin-right: auto;
+    margin-left: auto;
     list-style: none;
 
     .toast-message {
+      width: 100%;
+      max-width: max-content;
       padding: 10px 20px;
       color: white;
       background-color: rgba(0, 0, 0, 0.75);
