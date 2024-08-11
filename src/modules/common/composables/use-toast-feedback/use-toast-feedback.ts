1719256b29fd5025166f6ae66e19cb186997c1da
Francisco <javiermartincuenca@gmail.com>
2024-08-12 00:01:00 +0200

    feat: addtext

--- src/modules/common/composables/use-toast-feedback/use-toast-feedback.ts
+++ src/modules/common/composables/use-toast-feedback/use-toast-feedback.ts
@@ -1,7 +1,18 @@
 import { ref, type Ref } from 'vue';
 
-export default function useToastFeedback(): { messages: Ref<string[]> } {
-  const messages = ref<string[]>(['Hello world', 'Good']);
+export default function useToastFeedback(): {
+  messages: Ref<string[]>;
+  addToast: (text: string) => void;
+} {
+  const messages = ref<string[]>([
+    'Hello world',
+    'Good',
+    'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem voluptatibus earum itaque neque. Magnam dicta doloremque aut beatae, unde exercitationem esse enim explicabo. Obcaecati voluptate eligendi debitis quos amet impedit!',
+  ]);
 
-  return { messages };
+  function addToast(text: string): void {
+    messages.value.push(text);
+  }
+
+  return { messages, addToast };
 }
