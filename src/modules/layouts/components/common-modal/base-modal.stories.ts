2189f70de6126621d35cbbb42b66995abb7c6638
Francisco <javiermartincuenca@gmail.com>
2024-08-17 13:05:54 +0200

    docs: add story

---
+++ src/modules/layouts/components/common-modal/base-modal.stories.ts
@@ -0,0 +1,44 @@
+import { nextTick, onMounted, ref } from 'vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+import BaseModal from '@layouts/components/common-modal/common-modal.vue';
+
+const meta: Meta<typeof BaseModal> = {
+  title: 'Layouts/CommonModal',
+  component: BaseModal,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  args: {
+    showModal: true,
+  },
+  decorators: [
+    (args: object): object => ({
+      setup(): object {
+        const isOpen = ref<boolean>(false);
+
+        onMounted(() => {
+          nextTick();
+          isOpen.value = true;
+        });
+
+        return { args, isOpen };
+      },
+      template: `
+        <div>
+          <div id="screen" />
+          <div style="margin: 1rem">
+            <story>
+              <h1 style="color: red;">Base modal</h1>
+            </story>
+          </div>
+        </div>
+    `,
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
