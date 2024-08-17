e8c789a62f410d1c883ee3ad65dcfa5ad079bc8d
Francisco <javiermartincuenca@gmail.com>
2024-08-17 11:05:41 +0200

    feat: modal

---
+++ src/modules/layouts/components/common-modal/common-modal.vue
@@ -0,0 +1,79 @@
+<template>
+  <Transition name="fade" appear>
+    <div class="modal-overlay" v-if="showModal" @click="$emit('close')" />
+  </Transition>
+  <Transition name="pop" appear>
+    <dialog :open="showModal" class="modal" v-if="showModal">
+      <h1>Vue Transitions</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam distinctio vitae provident,
+        officia, nisi veritatis maiores officiis similique molestiae adipisci quaerat nulla
+        molestias ut sapiente dignissimos dolorum alias, nam optio.
+      </p>
+      <button @click="$emit('close')">Hide modal</button>
+    </dialog>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+defineProps(['showModal']);
+
+defineEmits(['close']);
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  /* position: absolute; */
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  text-align: center;
+  width: fit-content;
+  height: fit-content;
+  max-width: 75%;
+  padding: 2rem;
+  border-radius: 1rem;
+  background-color: white;
+  z-index: 20;
+  transform: none;
+}
+
+.modal-overlay {
+  content: '';
+  position: fixed;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 19;
+  background-color: rgb(0, 0, 0);
+  opacity: 0.8;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2.4s linear;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.pop-enter-active,
+.pop-leave-active {
+  transition:
+    transform 2.4s cubic-bezier(0.5, 0, 0.5, 1),
+    opacity 2.4s linear;
+}
+
+.pop-enter,
+.pop-leave-to {
+  opacity: 0;
+  transform: scale(0.3) translateY(-50%);
+}
+</style>
--- src/modules/layouts/components/menu-options/menu-options.vue
+++ src/modules/layouts/components/menu-options/menu-options.vue
@@ -2,12 +2,14 @@
   <button class="button blue menu-options-button" @click="handleShowOptions">
     <BaseIcon type="gear" />
   </button>
-  <MenuOptionsModal v-if="isOpen" @close="closeModal" />
+  <!-- <MenuOptionsModal v-if="isOpen" @close="closeModal" /> -->
+  <CommonModal :show-modal="isOpen" @close="isOpen = false" />
 </template>
 
 <script setup lang="ts">
 import { ref, type Ref } from 'vue';
 import MenuOptionsModal from '@options/components/menu-options-modal/menu-options-modal.vue';
+import CommonModal from '@layouts/components/common-modal/common-modal.vue';
 import BaseIcon from '@common/components/icons/base-icon.vue';
 
 //#region REFS
