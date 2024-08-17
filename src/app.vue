9ea532f20dc87f08362e3bd8ee252cb38a6a24bc
Francisco <javiermartincuenca@gmail.com>
2024-08-17 22:39:53 +0200

    feat: use locale

--- src/app.vue
+++ src/app.vue
@@ -3,8 +3,8 @@
     <RouterView />
   </AppWrapper> -->
   <div>
-    <h1 style="color: white">{{ $t('main.hello') }}</h1>
-    <select v-model="$i18n.locale">
+    <h1 style="color: white">{{ t('main.hello') }}</h1>
+    <select v-model="locale">
       <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang.code">
         {{ lang.text }}
       </option>
@@ -16,6 +16,8 @@
 import { RouterView } from 'vue-router';
 import AppWrapper from '@layouts/components/wrapper/app-wrapper/app-wrapper.vue';
 import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+const { t, locale } = useI18n();
 
 const langs = ref([
   { code: 'en', text: 'English' },
