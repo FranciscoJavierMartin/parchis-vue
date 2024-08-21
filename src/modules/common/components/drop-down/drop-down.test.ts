982bdc604c0c037ab5f3e94bc6f686be6f756a6a
Francisco <javiermartincuenca@gmail.com>
2024-08-21 17:49:56 +0200

    test: with options closed

--- src/modules/common/components/drop-down/drop-down.test.ts
+++ src/modules/common/components/drop-down/drop-down.test.ts
@@ -11,5 +11,32 @@ describe('drop-down.vue', () => {
     });
 
     expect(wrapper.find('.items').exists()).toBe(false);
+    expect(wrapper.find('.selected').exists()).toBe(true);
+    expect(wrapper.find('.selected').classes()).not.contains('open');
+  });
+
+  test('renders with options closed', () => {
+    const wrapper = mount(DropDown, {
+      props: {
+        options: [
+          {
+            value: 'option1',
+            data: {
+              text: 'Option 1',
+            },
+          },
+          {
+            value: 'option2',
+            data: {
+              text: 'Option 2',
+            },
+          },
+        ],
+      },
+    });
+
+    expect(wrapper.find('.items').exists()).toBe(false);
+    expect(wrapper.find('.selected').exists()).toBe(true);
+    expect(wrapper.find('.selected').classes()).not.contains('open');
   });
 });
