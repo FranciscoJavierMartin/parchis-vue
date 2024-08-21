04d536d3b7094845f408271e41d390d59db75a2d
Francisco <javiermartincuenca@gmail.com>
2024-08-22 00:29:19 +0200

    test: with icon

--- src/modules/common/components/input-switch/input-switch.test.ts
+++ src/modules/common/components/input-switch/input-switch.test.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import i18n from '@/i18n';
+import botImage from '@/assets/images/bot.png';
 import InputSwitch from './input-switch.vue';
 
 describe('input-switch.vue', () => {
@@ -59,5 +60,35 @@ describe('input-switch.vue', () => {
     expect(slider.exists()).toBe(true);
 
     expect(wrapper.find('.slider-icon').exists()).toBe(false);
+  });
+
+  test('with icon', () => {
+    const wrapper = mount(InputSwitch, {
+      props: {
+        id: 'abcd',
+        disabled: true,
+        icon: botImage,
+      },
+      global: {
+        plugins: [i18n],
+      },
+    });
+
+    const label = wrapper.find('.input-switch');
+
+    expect(label.exists()).toBe(true);
+    expect(label.attributes()['for']).toBe('abcd');
+    expect(label.attributes()['aria-label']).toBe('Player is bot');
+
+    const input = wrapper.find('#abcd');
+
+    expect(input.exists()).toBe(true);
+    expect(input.attributes()['disabled']).toEqual('');
+
+    const slider = wrapper.find('.slider');
+
+    expect(slider.exists()).toBe(true);
+
+    expect(wrapper.find('.slider-icon').exists()).toBe(true);
   });
 });
