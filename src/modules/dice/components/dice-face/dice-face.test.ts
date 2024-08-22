abbc1d522d1b33f6825c4d9788f020c14af18c65
Francisco <javiermartincuenca@gmail.com>
2024-08-22 23:56:19 +0200

    test: dice face

--- src/modules/dice/components/dice-face/dice-face.test.ts
+++ src/modules/dice/components/dice-face/dice-face.test.ts
@@ -9,6 +9,33 @@ describe('dice-face.vue', () => {
       props: { value: value as TDiceValues },
     });
 
-    expect(wrapper.find(`.dice-face.dice-${value}`).exists()).toBe(true);
+    const face = wrapper.find(`.dice-face.dice-${value}`);
+    expect(face.exists()).toBe(true);
+    expect(face.classes()).not.contains('animate');
+    expect(face.classes()).not.contains('shadow');
+    expect(face.exists()).toBe(true);
+    expect(getComputedStyle(face.element).width).toBe('29.9px');
+  });
+
+  test('custom dice size', () => {
+    const wrapper = mount(Dice3d, {
+      props: { value: 1, size: 40 },
+    });
+    const face = wrapper.find('.dice-face');
+    expect(getComputedStyle(face.element).width).toBe('40px');
+  });
+
+  test('has animate class', () => {
+    const wrapper = mount(Dice3d, {
+      props: { value: 1, animate: true },
+    });
+    expect(wrapper.find('.dice-face.animate').exists()).toBe(true);
+  });
+
+  test('has shadow class', () => {
+    const wrapper = mount(Dice3d, {
+      props: { value: 1, shadow: true },
+    });
+    expect(wrapper.find('.dice-face.shadow').exists()).toBe(true);
   });
 });
