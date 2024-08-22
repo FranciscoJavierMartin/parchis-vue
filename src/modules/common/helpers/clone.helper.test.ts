ff168117129fb999fee54f97dc77b8297d3a1af3
Francisco <javiermartincuenca@gmail.com>
2024-08-22 18:07:07 +0200

    test: clone object

---
+++ src/modules/common/helpers/clone.helper.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'vitest';
+import { cloneDeep } from './clone.helper';
+
+describe('cloneDeep', () => {
+  test('clone object', () => {
+    const original = {
+      a: 1,
+      b: {
+        c: 2,
+      },
+    };
+
+    const copy = cloneDeep(original);
+
+    expect(copy).not.toBe(original);
+    expect(copy === original).toBe(false);
+    expect(copy).toStrictEqual(original);
+  });
+});
