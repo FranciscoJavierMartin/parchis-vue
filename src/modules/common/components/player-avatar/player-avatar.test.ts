b41ffc553347556c27af4b58eb637e92ff4ea43a
Francisco <javiermartincuenca@gmail.com>
2024-08-22 17:10:34 +0200

    test: render properly

---
+++ src/modules/common/components/player-avatar/player-avatar.test.ts
@@ -0,0 +1,16 @@
+import { describe, test, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import botImage from '@/assets/images/bot.png';
+import PlayerAvatar from './player-avatar.vue';
+
+describe('player-avatar.vue', () => {
+  test('renders properly', () => {
+    const wrapper = mount(PlayerAvatar);
+
+    const img = wrapper.find('img');
+    img.trigger('error');
+    expect(img.exists()).toBe(true);
+    expect(img.attributes()['src']).contains('assets/images/default.png');
+    expect(img.attributes()['alt']).toBe('');
+  });
+});
