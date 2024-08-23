3eff7b0a41667be47c73db8d1df39a0367e9a2f6
Francisco <javiermartincuenca@gmail.com>
2024-08-23 21:12:14 +0200

    test: other player test

---
+++ src/modules/game/components/over/other-player/other-player.test.ts
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils';
+import { describe, expect, test } from 'vitest';
+import i18n from '@/i18n';
+import type { IPlayer } from '@players/interfaces/user.interface';
+import botImage from '@/assets/images/bot.png';
+import OtherPlayer from './other-player.vue';
+
+describe('other-player.vue', () => {
+  test('renders properly', () => {
+    const wrapper = mount(OtherPlayer, {
+      props: {
+        player: {
+          ranking: 2,
+          name: 'Alice',
+          photo: botImage,
+          color: 'RED',
+        } as IPlayer,
+      },
+      global: {
+        plugins: [i18n],
+      },
+    });
+
+    expect(wrapper.find('.other-player-container').exists()).toBe(true);
+
+    expect(wrapper.find('.position').text()).toBe('2nd');
+    expect(wrapper.find('.ranking-player-name.red').exists()).toBe(true);
+    expect(wrapper.find('.ranking-player-name').text()).toBe('Alice');
+  });
+});
