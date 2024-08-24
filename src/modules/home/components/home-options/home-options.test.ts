0e341ee415bb2eecc60a911e1e1517467ec769c1
Francisco <javiermartincuenca@gmail.com>
2024-08-24 18:21:00 +0200

    test: navigation

--- src/modules/home/components/home-options/home-options.test.ts
+++ src/modules/home/components/home-options/home-options.test.ts
@@ -1,8 +1,9 @@
 import { describe, expect, test } from 'vitest';
-import { mount } from '@vue/test-utils';
-import HomeOptions from './home-options.vue';
+import { flushPromises, mount, RouterLinkStub } from '@vue/test-utils';
 import router from '@/router';
 import i18n from '@/i18n';
+import { ROUTES } from '@/router/routes';
+import HomeOptions from './home-options.vue';
 
 describe('home-options.vue', () => {
   test('renders properly', () => {
@@ -41,5 +42,33 @@ describe('home-options.vue', () => {
 
     expect(wrapper.find('.home-options .button.yellow').exists()).toBe(true);
     expect(wrapper.find('.home-options .button.yellow').text()).toBe('Play offline');
+  });
+
+  test('navigate to online page', async () => {
+    const wrapper = mount(HomeOptions, {
+      global: {
+        plugins: [i18n, router],
+        stubs: {
+          RouterLink: RouterLinkStub,
+        },
+      },
+    });
+
+    const link = wrapper.findComponent('.button.blue') as any;
+    expect(link.props().to).toEqual({ name: ROUTES.ONLINE.name });
+  });
+
+  test('navigate to offline page', async () => {
+    const wrapper = mount(HomeOptions, {
+      global: {
+        plugins: [i18n, router],
+        stubs: {
+          RouterLink: RouterLinkStub,
+        },
+      },
+    });
+
+    const link = wrapper.findComponent('.button.yellow') as any;
+    expect(link.props().to).toEqual({ name: ROUTES.OFFLINE.name });
   });
 });
