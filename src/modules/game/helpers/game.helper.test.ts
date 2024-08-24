f462ab782f98073425f7defcd57deafc6b984fdd
Francisco <javiermartincuenca@gmail.com>
2024-08-24 13:08:01 +0200

    test: online players

--- src/modules/game/helpers/game.helper.test.ts
+++ src/modules/game/helpers/game.helper.test.ts
@@ -38,4 +38,42 @@ describe('getInitialActionsTurnValue', () => {
       actionsBoardGame: EActionsBoardGame.ROLL_DICE,
     });
   });
+
+  test('online players', () => {
+    const actionsTurn = getInitialActionsTurnValue(0, [
+      {
+        color: 'BLUE',
+        counterMessage: 0,
+        finished: false,
+        id: 'asdf',
+        index: 1,
+        isOffline: false,
+        isOnline: true,
+        name: 'Alice',
+        ranking: 1,
+      },
+      {
+        color: 'YELLOW',
+        counterMessage: 0,
+        finished: false,
+        id: 'qwerty',
+        index: 0,
+        isOffline: false,
+        isOnline: true,
+        name: 'Bob',
+        ranking: 1,
+      },
+    ]);
+
+    expect(actionsTurn).toEqual({
+      timerActivated: true,
+      disabledDice: false,
+      showDice: true,
+      diceValue: 0,
+      diceList: [],
+      diceRollNumber: 0,
+      isDisabledUI: false,
+      actionsBoardGame: EActionsBoardGame.ROLL_DICE,
+    });
+  });
 });
