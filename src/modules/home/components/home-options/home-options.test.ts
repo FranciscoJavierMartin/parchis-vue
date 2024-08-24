0cf2ab2392beaa27e679a799b9f3e3e2f6f18baf
Francisco <javiermartincuenca@gmail.com>
2024-08-24 13:31:09 +0200

    test: renders properly

---
+++ src/modules/home/components/home-options/home-options.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test } from 'vitest';
+import { mount } from '@vue/test-utils';
+import HomeOptions from './home-options.vue';
+import router from '@/router';
+import i18n from '@/i18n';
+
+describe('home-options.vue', () => {
+  test('renders properly', () => {
+    const wrapper = mount(HomeOptions, {
+      global: {
+        plugins: [i18n, router],
+      },
+    });
+
+    expect(wrapper.find('.home-options').exists()).toBe(true);
+    expect(wrapper.find('.home-options .button.blue').exists()).toBe(true);
+    expect(wrapper.find('.home-options .button.blue .icon-online').exists()).toBe(true);
+    expect(wrapper.find('.home-options .button.blue').text()).toBe('Play online');
+
+    expect(wrapper.find('.home-options .button.yellow').exists()).toBe(true);
+    expect(wrapper.find('.home-options .button.yellow .icon-offline').exists()).toBe(true);
+    expect(
+      wrapper.find('.home-options .button.yellow .icon-offline svg path').attributes()['fill'],
+    ).toBe('#856404');
+
+    expect(wrapper.find('.home-options .button.yellow').text()).toBe('Play offline');
+  });
+});
