4a5dc1f4ad91f9e06da5d1d68f97d30f91a3eb11
Francisco <javiermartincuenca@gmail.com>
2024-08-25 17:20:32 +0200

    test: online view

--- src/modules/online/views/online-view.test.ts
+++ src/modules/online/views/online-view.test.ts
@@ -3,14 +3,9 @@ import { mount } from '@vue/test-utils';
 import i18n from '@/i18n';
 import router from '@/router';
 import { OptionsGameStateSymbol } from '@sounds/constants/sounds.constants';
-import BackButton from '@layouts/components/back-button/back-button/back-button.vue';
-import MenuOptions from '@layouts/components/menu-options/menu-options.vue';
-import Game from '@game/components/game/game.vue';
-import GameLogo from '@common/components/game-logo/game-logo.vue';
-import SetupPlayers from '@players/components/setup-players/setup-players.vue';
-import OfflineView from './offline-view.vue';
+import OfflineView from './online-view.vue';
 
-describe('offline-view.vue', () => {
+describe('online-view.vue', () => {
   beforeEach(() => {
     // create teleport target
     const el = document.createElement('div');
@@ -37,10 +32,7 @@ describe('offline-view.vue', () => {
     });
 
     expect(wrapper.find('.page-wrapper').exists()).toBe(true);
-    expect(wrapper.findComponent(BackButton).exists()).toBe(true);
-    expect(wrapper.findComponent(MenuOptions).exists()).toBe(true);
-    expect(wrapper.findComponent(Game).exists()).toBe(false);
-    expect(wrapper.findComponent(GameLogo).exists()).toBe(true);
-    expect(wrapper.findComponent(SetupPlayers).exists()).toBe(true);
+    expect(wrapper.find('.online-page').exists()).toBe(true);
+    expect(wrapper.find('h1').text()).toBe('Online');
   });
 });
