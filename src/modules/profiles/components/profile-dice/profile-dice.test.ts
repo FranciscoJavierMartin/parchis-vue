1f3a221084c20a52591ecddad482055ab3e2c040
Francisco <javiermartincuenca@gmail.com>
2024-08-26 22:42:16 +0200

    test: renders properly

---
+++ src/modules/profiles/components/profile-dice/profile-dice.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from 'vitest';
+import { mount } from '@vue/test-utils';
+import Dice3d from '@dice/components/dice-3d/dice-3d.vue';
+import ProfileDice from './profile-dice.vue';
+
+describe('profile-dice.vue', () => {
+  test('renders properly', () => {
+    const wrapper = mount(ProfileDice, {
+      props: {
+        diceRollNumber: 0,
+        disabledDice: false,
+        showDice: true,
+        value: 1,
+      },
+    });
+
+    expect(wrapper.find('.game-profile-dice').exists()).toBe(true);
+    expect(wrapper.find('.game-profile-dice').classes()).not.toContain('hide');
+    expect(wrapper.find('.icon-arrow').exists()).toBe(true);
+    expect(wrapper.find('button.game-profile-dice-button').exists()).toBe(true);
+    expect(wrapper.findComponent(Dice3d).exists()).toBe(true);
+  });
+});
