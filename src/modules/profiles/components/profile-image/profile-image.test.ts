dbb79a79fb8115ed0346f0c2cd9d08e41f6e59e1
Francisco <javiermartincuenca@gmail.com>
2024-08-26 23:11:41 +0200

    test: renders properly

---
+++ src/modules/profiles/components/profile-image/profile-image.test.ts
@@ -0,0 +1,36 @@
+import { describe, expect, test } from 'vitest';
+import { mount } from '@vue/test-utils';
+import i18n from '@/i18n';
+import PlayerAvatar from '@common/components/player-avatar/player-avatar.vue';
+import ProfileImage from './profile-image.vue';
+
+describe('profile-image.vue', () => {
+  test('renders properly', () => {
+    const wrapper = mount(ProfileImage, {
+      props: {
+        player: {
+          color: 'BLUE',
+          finished: false,
+          id: 'asdf',
+          index: 1,
+          name: 'Alice',
+          counterMessage: 0,
+          isOffline: false,
+          ranking: 1,
+          isOnline: true,
+        },
+        startTimer: false,
+        position: 'LEFT',
+      },
+      global: {
+        plugins: [i18n],
+      },
+    });
+
+    expect(wrapper.find('.game-profile-image').exists()).toBe(true);
+    expect(wrapper.find('.game-profile-image-offline').exists()).toBe(false);
+    expect(wrapper.findComponent(PlayerAvatar).exists()).toBe(true);
+    expect(wrapper.find('button.game-profile-mute-chat').exists()).toBe(true);
+    expect(wrapper.find('.game-profile-image-progress').exists()).toBe(false);
+  });
+});
