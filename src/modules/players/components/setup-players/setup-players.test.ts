4280ca4c3f744d5754b570f2170aa3d47ff8dfff
Francisco <javiermartincuenca@gmail.com>
2024-08-26 19:38:27 +0200

    test: skip change number of players

--- src/modules/players/components/setup-players/setup-players.test.ts
+++ src/modules/players/components/setup-players/setup-players.test.ts
@@ -4,6 +4,7 @@ import i18n from '@/i18n';
 import SelectNumberPlayers from '@players/components/select-number-players/select-number-players.vue';
 import SetupPlayers from './setup-players.vue';
 import PlayerInput from '../player-input/player-input.vue';
+import { nextTick } from 'vue';
 
 describe('setup-players.vue', () => {
   test('renders properly', () => {
@@ -20,5 +21,19 @@ describe('setup-players.vue', () => {
     expect(wrapper.find('button[type="submit"]').text()).toBe('Play');
     expect(wrapper.find('button .icon-play').exists()).toBe(true);
     expect(wrapper.find('button .icon-play svg path').attributes()['fill']).toBe('#8b5f00');
+  });
+
+  test.skip('change number of players', async () => {
+    const wrapper = mount(SetupPlayers, {
+      global: {
+        plugins: [i18n],
+      },
+    });
+
+    expect(wrapper.find('.game-offline-players').findAllComponents(PlayerInput)).toHaveLength(4);
+    expect(wrapper.find('label[for="2P"]').exists()).toBe(true);
+    await wrapper.find('label[for="2P"]').trigger('click');
+    await nextTick();
+    expect(wrapper.find('.game-offline-players').findAllComponents(PlayerInput)).toHaveLength(2);
   });
 });
