a3c6af171b2c7f8d9339e66b52514185bc476afc
Francisco <javiermartincuenca@gmail.com>
2024-08-26 23:03:06 +0200

    test: emit done dice

--- src/modules/profiles/components/profile-dice/profile-dice.test.ts
+++ src/modules/profiles/components/profile-dice/profile-dice.test.ts
@@ -1,9 +1,17 @@
-import { describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 import { mount } from '@vue/test-utils';
 import Dice3d from '@dice/components/dice-3d/dice-3d.vue';
 import ProfileDice from './profile-dice.vue';
 
 describe('profile-dice.vue', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.clearAllTimers();
+  });
+
   test('renders properly', () => {
     const wrapper = mount(ProfileDice, {
       props: {
@@ -46,5 +54,23 @@ describe('profile-dice.vue', () => {
 
     await wrapper.find('button').trigger('click');
     expect(wrapper.emitted()).toHaveProperty('handleSelectDice');
+  });
+
+  test('emit done dice', async () => {
+    const wrapper = mount(ProfileDice, {
+      props: {
+        diceRollNumber: 0,
+        disabledDice: false,
+        showDice: true,
+        value: 0,
+      },
+    });
+
+    await wrapper.find('button').trigger('click');
+
+    await wrapper.setProps({ value: 4, diceRollNumber: 1 });
+    vi.advanceTimersByTime(1_000);
+
+    expect(wrapper.emitted()).toHaveProperty('handleDoneDice');
   });
 });
