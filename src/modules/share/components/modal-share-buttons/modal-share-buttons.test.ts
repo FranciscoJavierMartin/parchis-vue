085d84c9244131f0e255acca1882cbae760d8ce6
Francisco <javiermartincuenca@gmail.com>
2024-08-27 20:35:27 +0200

    test: render buttons

--- src/modules/share/components/modal-share-buttons/modal-share-buttons.test.ts
+++ src/modules/share/components/modal-share-buttons/modal-share-buttons.test.ts
@@ -21,4 +21,27 @@ describe('modal-share-buttons.vue', () => {
     expect(wrapper.find('.modal-share-buttons').exists()).toBe(true);
     expect(wrapper.findAll('.modal-share-button')).toHaveLength(4);
   });
+
+  test.for([
+    ['Share in Copy', 'copy'],
+    ['Share in X (Twitter)', 'twitter'],
+    ['Share in Facebook', 'facebook'],
+    ['Share in Linkedin', 'linkedin'],
+  ])('renders %s button', ([buttonTitle, icon]) => {
+    const wrapper = mount(ModalShareButtons, {
+      props: {
+        data: {
+          text: 'Text test',
+          title: 'Title test',
+          url: 'my-url',
+        },
+      },
+      global: {
+        plugins: [i18n],
+      },
+    });
+
+    expect(wrapper.find(`button[title="${buttonTitle}"]`).exists()).toBe(true);
+    expect(wrapper.find(`button .icon-${icon}`).exists()).toBe(true);
+  });
 });
