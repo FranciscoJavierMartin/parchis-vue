d49804c2b182e296aaa4f4f5ce41f7fbb185a2b8
Francisco <javiermartincuenca@gmail.com>
2024-08-27 20:13:59 +0200

    test: doesnt renders

--- src/modules/profiles/layouts/profile-wrapper/profile-wrapper.test.ts
+++ src/modules/profiles/layouts/profile-wrapper/profile-wrapper.test.ts
@@ -51,4 +51,51 @@ describe('profile-wrapper.vue', () => {
 
     expect(wrapper.findComponent(ProfilePlayer).exists()).toBe(true);
   });
+
+  test('doesnt renders', () => {
+    const wrapper = mount(ProfileWrapper, {
+      props: {
+        position: 'RIGHT',
+        actionsTurn: {
+          diceList: [],
+          diceRollNumber: 0,
+          diceValue: 0,
+          disabledDice: false,
+          isDisabledUI: false,
+          showDice: false,
+          timerActivated: false,
+        },
+        basePosition: 'BOTTOM',
+        currentTurn: 1,
+        players: [
+          {
+            color: 'BLUE',
+            counterMessage: 0,
+            finished: false,
+            id: 'qwerty',
+            index: 0,
+            isOffline: false,
+            name: 'Alice',
+            ranking: 1,
+          },
+          {
+            color: 'RED',
+            counterMessage: 0,
+            finished: false,
+            id: 'zxcvb',
+            index: 0,
+            isOffline: false,
+            name: 'Bob',
+            ranking: 1,
+          },
+        ],
+        totalPlayers: 2,
+      },
+      global: {
+        plugins: [i18n],
+      },
+    });
+
+    expect(wrapper.findComponent(ProfilePlayer).exists()).toBe(false);
+  });
 });
