881ee8f897ae626f2d755846aa2531f5331a7f02
Francisco <javiermartincuenca@gmail.com>
2024-08-27 18:34:15 +0200

    test: show position in ranking

--- src/modules/profiles/components/profile-player/profile-player.test.ts
+++ src/modules/profiles/components/profile-player/profile-player.test.ts
@@ -4,6 +4,7 @@ import i18n from '@/i18n';
 import ProfileImage from '@profiles/components/profile-image/profile-image.vue';
 import ProfileDice from '@profiles/components/profile-dice/profile-dice.vue';
 import ProfileRanking from '@profiles/components/profile-ranking/profile-ranking.vue';
+import NameAndDice from '@profiles/components/name-and-dice/name-and-dice.vue';
 import ProfilePlayer from './profile-player.vue';
 
 describe('profile-player.vue', () => {
@@ -43,6 +44,7 @@ describe('profile-player.vue', () => {
     expect(wrapper.find('.game-profile').classes()).toContain('left');
     expect(wrapper.find('.game-profile-dice-name').exists()).toBe(true);
     expect(wrapper.findComponent(ProfileImage).exists()).toBe(true);
+    expect(wrapper.findComponent(NameAndDice).exists()).toBe(true);
     expect(wrapper.findComponent(ProfileDice).exists()).toBe(false);
     expect(wrapper.findComponent(ProfileRanking).exists()).toBe(false);
   });
@@ -79,5 +81,39 @@ describe('profile-player.vue', () => {
     });
 
     expect(wrapper.findComponent(ProfileDice).exists()).toBe(true);
+  });
+
+  test('show position in ranking', () => {
+    const wrapper = mount(ProfilePlayer, {
+      props: {
+        basePosition: 'BOTTOM',
+        hasTurn: true,
+        position: 'LEFT',
+        player: {
+          color: 'BLUE',
+          name: 'Alice',
+          counterMessage: 0,
+          finished: true,
+          id: 'asdfg',
+          index: 1,
+          isOffline: false,
+          ranking: 1,
+        },
+        actionsTurn: {
+          diceList: [],
+          diceRollNumber: 0,
+          diceValue: 0,
+          disabledDice: false,
+          isDisabledUI: false,
+          showDice: false,
+          timerActivated: false,
+        },
+      },
+      global: {
+        plugins: [i18n],
+      },
+    });
+
+    expect(wrapper.findComponent(ProfileRanking).exists()).toBe(true);
   });
 });
