2bbd896b9c6ec829d853870401b079617c8d6f58
Francisco <javiermartincuenca@gmail.com>
2024-08-31 12:43:41 +0200

    test: renders tokens color

--- src/modules/tokens/components/token/token-piece/token-piece.test.ts
+++ src/modules/tokens/components/token/token-piece/token-piece.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, test } from 'vitest';
 import { mount } from '@vue/test-utils';
+import { EColors } from '@board/interfaces/board.enum';
+import type { TColors } from '@board/interfaces/board.interface';
 import TokenPiece from './token-piece.vue';
 
 describe('token-piece.vue', () => {
@@ -12,5 +14,15 @@ describe('token-piece.vue', () => {
 
     expect(wrapper.find('.game-token-piece').exists()).toBe(true);
     expect(wrapper.find('.game-token-piece span').exists()).toBe(false);
+  });
+
+  test.for(Object.keys(EColors) as TColors[])('renders %s token', (color: TColors) => {
+    const wrapper = mount(TokenPiece, {
+      props: {
+        color,
+      },
+    });
+
+    expect(wrapper.find('.game-token-piece').classes()).toContain(color.toLowerCase());
   });
 });
