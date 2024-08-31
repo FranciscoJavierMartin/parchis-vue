2e717822978ea3a730142a0f4de4c512ed04bb37
Francisco <javiermartincuenca@gmail.com>
2024-08-31 18:50:09 +0200

    fix: fix debounce test

--- src/modules/common/helpers/debounce.helper.test.ts
+++ src/modules/common/helpers/debounce.helper.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expect, Mock, test, vi, vitest } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 import debounce, { delay } from './debounce.helper';
 
 describe('delay', () => {
@@ -7,7 +7,7 @@ describe('delay', () => {
     await delay(200);
     const currentTime: number = Date.now();
 
-    expect(currentTime - previousTime).toBeLessThanOrEqual(210);
+    expect(currentTime - previousTime).toBeLessThanOrEqual(250);
     expect(currentTime - previousTime).toBeGreaterThanOrEqual(200);
   });
 });
