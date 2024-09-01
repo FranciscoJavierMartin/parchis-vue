df036452e1f02f0749dab039701b67c02996a204
Francisco <javiermartincuenca@gmail.com>
2024-09-01 14:05:09 +0200

    fix: add default value

--- src/modules/home/views/home-view.vue
+++ src/modules/home/views/home-view.vue
@@ -1,15 +1,10 @@
 <template>
   <PageWrapper>
     <template #default>
-      <!-- <GameLogo />
+      <GameLogo />
       <HomeOptions :serviceError="serviceError" />
       <HomeAlert v-if="serviceError" />
-      <HomeToolbar :isAuth="isAuth" /> -->
-      <div>
-        <MyInput v-model="myName" />
-        <input v-model="myName" placeholder="Name" />
-        <h1>{{ myName }}</h1>
-      </div>
+      <HomeToolbar :isAuth="isAuth" />
     </template>
     <template #rightOption>
       <MenuOptions />
@@ -24,11 +19,7 @@ import MenuOptions from '@layouts/components/menu-options/menu-options.vue';
 import HomeOptions from '@home/components/home-options/home-options.vue';
 import HomeAlert from '@home/components/home-alert/home-alert.vue';
 import HomeToolbar from '@home/components/home-toolbar/home-toolbar.vue';
-import MyInput from './my-input.vue';
-import { ref } from 'vue';
 
 const serviceError: boolean = false;
 const isAuth: boolean = true;
-
-const myName = ref<string>('');
 </script>
